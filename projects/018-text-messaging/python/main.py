b41cf8b9ef3e9422b89c4ef5f6ed88946d52d6ce
EleSar <sarnicola.eleonora@gmail.com>
2025-03-21 22:48:15 +0100

    optimized text_message_to_keys logic

--- projects/018-text-messaging/python/main.py
+++ projects/018-text-messaging/python/main.py
@@ -5,29 +5,21 @@ def text_message_to_keys(message: str="") -> str:
     
     if message == "":
         return message
-
-    phone_keypad = {"1": ".,?!:",
-                    "2": "ABC",
-                    "3": "DEF",
-                    "4": "GHI",
-                    "5": "JKL",
-                    "6": "MNO",
-                    "7": "PQRS",
-                    "8": "TUV",
-                    "9": "WXYZ",
-                    "0": " "}
+    
+    phone_keypad = {".": "1", ",": "11", "?": "111", "!": "1111", ":": "11111", "1": "111111",
+                    "A": "2", "B": "22", "C": "222", "2": "2222", "D": "3", "E": "33",
+                    "F": "333", "3": "3333", "G": "4", "H": "44", "I": "444", "4": "4444", 
+                    "J": "5", "K": "55", "L": "555", "5": "5555", "M": "6", "N": "66", "O": "666",
+                    "6": "6666", "P": "7", "Q": "77", "R": "777", "S": "7777", "7": "77777",
+                    "T": "8", "U": "88", "V": "888", "8": "8888", "W": "9", "X": "99", "Y": "999",
+                    "Z": "9999", "9": "99999",  " " : "0", "0": "00"}
     
     pressed_keys = ""
-    case_insensitive_msg = message.upper()
+    upper_msg = message.upper()
 
-    for char in case_insensitive_msg:    
-        for phone_key, letters in phone_keypad.items():
-            if char in letters:    
-                pressed_keys += phone_key * (letters.index(char) + 1)
-                break   #avoid useless iterations
-            elif char == phone_key:
-                pressed_keys += phone_key * (len(letters) + 1)
-                break
+    for char in upper_msg:    
+        if char in phone_keypad:
+            pressed_keys += phone_keypad[char]
 
     return pressed_keys
 
